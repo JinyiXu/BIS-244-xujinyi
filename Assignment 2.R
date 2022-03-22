bc039f6baa9b0df2c0390b2dc9a3e2f167de217e
Troy Adair <tra418@lehigh.edu>
2022-03-21 21:19:10 -0400

    Assignment 2 graded

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,4 +1,5 @@
 library(dplyr)
+# TAA Data should be in submodule
 US <- read.csv("us-states.csv")
 Penn <- filter(US,state=="Pennsylvania")
 View(Penn)
@@ -8,6 +9,7 @@ incr_cases <- 1
 for (i in 2:n) {
   incr_cases[i] <- (Penn$cases[i]-Penn$cases[i-1]) 
 }
+# TAA Row 1 incr should be equal to level
 incr_cases
 sum(incr_cases)
 sd(incr_cases)
@@ -16,6 +18,7 @@ incr_deaths <- 1
 for (i in 2:n) {
   incr_deaths[i] <- (Penn$deaths[i]-Penn$deaths[i-1]) 
 }
+# TAA Row 1 incr should be equal to level
 incr_deaths
 sum(incr_deaths)
 
